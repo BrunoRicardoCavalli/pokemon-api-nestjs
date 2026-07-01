5867b45bb38d25b42ad047ef08394f28cc32b07f
BrunoRicardoCavalli <73122526+BrunoRicardoCavalli@users.noreply.github.com>
2026-07-01 15:30:01 -0300

    feat: configure http module

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
+import { HttpModule } from '@nestjs/axios';
+
 import { PokemonController } from './pokemon.controller';
+import { PokemonService } from './pokemon.service';
 
 @Module({
+  imports: [HttpModule],
+
   controllers: [PokemonController],
+
   providers: [PokemonService],
 })
 export class PokemonModule {}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  findAll() {
+    return {
+      message: 'Lista de Pokémons funcionando!',
+    };
+  }
+}
