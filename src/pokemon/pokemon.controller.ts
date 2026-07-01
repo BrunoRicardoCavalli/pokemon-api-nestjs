06684a833fffe82d6e736789f9dd78e35c78b94b
BrunoRicardoCavalli <73122526+BrunoRicardoCavalli@users.noreply.github.com>
2026-07-01 16:41:53 -0300

    feat: integrate pokemon search with PokeAPI

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
-  @Get()
-  findAll() {
-    return this.pokemonService.findAll();
+  @Get(':nameOrId')
+  findOne(@Param('nameOrId') nameOrId: string) {
+    return this.pokemonService.findOne(nameOrId);
   }
 }
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,10 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class PokemonService {
-  findAll() {
-    return {
-      message: 'Lista de Pokémons funcionando!',
-    };
+  constructor(private readonly httpService: HttpService) {}
+
+  async findOne(nameOrId: string) {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.get(
+          `https://pokeapi.co/api/v2/pokemon/${nameOrId.toLowerCase()}`,
+        ),
+      );
+
+      return {
+        id: response.data.id,
+        name: response.data.name,
+        height: response.data.height,
+        weight: response.data.weight,
+        types: response.data.types.map((item) => item.type.name),
+        abilities: response.data.abilities.map((item) => item.ability.name),
+      };
+    } catch {
+      throw new NotFoundException('Pokémon não encontrado');
+    }
   }
 }
